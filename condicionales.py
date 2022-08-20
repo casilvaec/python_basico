53f970352d0d5feb02de1892e929e0b796ddd924
carlos-ubuntu <casilva2@utpl.edu.ec>
2022-08-19 19:04:27 -0500

    practicando condicionales

---
+++ condicionales.py
@@ -0,0 +1,16 @@
+# Practicando condicionales
+edad = int(input("Ingresa tu edad: "))#El texto ingresado se convierte a número
+if edad > 17:
+    print("Eres mayor de edad") 
+else:
+    print("Eres menor de edad")
+
+
+numero = int(input("Ingresa un número: "))
+
+if numero > 10:
+    print("Es mayor a 10")
+elif numero == 10:
+    print("Es igual a 10")
+else:
+    print("Es menor a 10")
